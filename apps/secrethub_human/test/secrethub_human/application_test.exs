e6f5d7200b711dc9b2755be53b957ebafdb749e8
Jonathan Gao <gsmlg.com@gmail.com>
2026-08-11 00:59:23 +0800

    test(human): cover application enablement config

--- apps/secrethub_human/test/secrethub_human/application_test.exs
+++ apps/secrethub_human/test/secrethub_human/application_test.exs
@@ -1,7 +1,18 @@
 defmodule SecretHub.Human.ApplicationTest do
-  use ExUnit.Case, async: true
+  use ExUnit.Case, async: false
 
   alias SecretHub.Human.Application, as: HumanApplication
+
+  setup do
+    enabled = Application.fetch_env(:secrethub_human, :enabled)
+
+    on_exit(fn ->
+      case enabled do
+        {:ok, value} -> Application.put_env(:secrethub_human, :enabled, value)
+        :error -> Application.delete_env(:secrethub_human, :enabled)
+      end
+    end)
+  end
 
   test "starts the Human supervision tree when enabled" do
     assert HumanApplication.children(enabled: true) == [
@@ -13,6 +24,22 @@ defmodule SecretHub.Human.ApplicationTest do
 
   test "starts no Human children when disabled" do
     assert HumanApplication.children(enabled: false) == []
+  end
+
+  test "uses the configured enablement when no option is given" do
+    Application.put_env(:secrethub_human, :enabled, false)
+
+    assert HumanApplication.children() == []
+  end
+
+  test "defaults to enabled when enablement is not configured" do
+    Application.delete_env(:secrethub_human, :enabled)
+
+    assert HumanApplication.children() == [
+             SecretHub.Human.Repo,
+             {Phoenix.PubSub, name: SecretHub.Human.PubSub},
+             SecretHub.HumanWeb.Endpoint
+           ]
   end
 
   test "does not add the Human application to the Core runtime" do
